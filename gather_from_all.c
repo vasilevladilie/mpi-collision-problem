f3c2d7bff063359cf3eac8b11e887d19dc923214
vlad.ilie <vasilevladilie@gmail.com>
2020-06-21 17:42:11 -0400

    [00411222062020] Implement discrete collision detection

--- gather_from_all.c
+++ gather_from_all.c
@@ -4,8 +4,35 @@
 #include <time.h>
 #include <unistd.h>
 #include <math.h>
+#include <vector>
+#include <tuple>
+#include <chrono>
+#include <iostream>
 
 bool continueGatheringPossibleCollisions = false;
+
+float GenerateNonNullPrimeNumber()
+{
+  const int kNumberOfDecimals = std::pow(10, 5);
+  float x = 0.0;
+  const float kNull = 0.0;
+  while (kNull == x)
+  {
+    x = static_cast<float>(rand()%kNumberOfDecimals)/static_cast<float>(kNumberOfDecimals);
+  }
+  return x;
+}
+
+std::vector<std::tuple<int, int> > collisions;
+bool IsPreviouslyFoundCollision(int rank, int worldRank)
+{
+  for (int i = 0; i < collisions.size(); ++i)
+  {
+    if (std::get<0>(collisions[i]) == rank && std::get<1>(collisions[i]) == worldRank)
+      return true;
+  }
+  return false;
+}
 
 int main(int argc, char** argv)
 {
@@ -19,80 +46,65 @@ int main(int argc, char** argv)
   MPI_Comm_rank(MPI_COMM_WORLD, &worldRank);
 
   continueGatheringPossibleCollisions = true;
-  const int kNumberOfDecimals = std::pow(10, 5);
   float* objectsData = nullptr;
   float* readBuf = (float*)malloc(worldSize*4*sizeof(float));
- 
+
+  float* sphereSizes = new float[worldSize];
+  for (int i = 0; i < worldSize; ++i)
+  {
+    sphereSizes[i] = GenerateNonNullPrimeNumber() * 1/20; 
+  }
+
+  auto collisionStart = std::chrono::high_resolution_clock::now();
   while(continueGatheringPossibleCollisions)
   {
+    float myRankX(0), myRankY(0), myRankZ(0);
     if (worldRank == 0)
     {
       objectsData = new float[worldSize * 4];
       srand(time(NULL));
-      float* kSphereRadiuses = new float[worldSize];
       for (int i = 0; i < worldSize; ++i)
       {
-	kSphereRadiuses[i] = static_cast<float>(i + 1);
-
-	printf("%d Sphere radius %.5f\n", i, kSphereRadiuses[i]);
-      }
-
-      for (int i = 0; i < worldSize; ++i)
-      {
-	float x = 0;
-	while(x == 0)
-	{
-	  x = static_cast<float>(rand()%kNumberOfDecimals)/static_cast<float>(kNumberOfDecimals);
-        }
-	// usleep(100);
-	float y = 0;
-	while(y == 0)
-	{
-	  y = static_cast<float>(rand()%kNumberOfDecimals)/static_cast<float>(kNumberOfDecimals);
-	}
-	// usleep(100);
-	float z = 0;
-	while(z == 0)
-	{
-	  z = static_cast<float>(rand()%kNumberOfDecimals)/static_cast<float>(kNumberOfDecimals);
-	}
-	// usleep(100);
-    
-	objectsData[i*4] = kSphereRadiuses[i];
-	printf("objectsData[%d] = %.5f\n", i, kSphereRadiuses[i]);
-	
+	float x = GenerateNonNullPrimeNumber();
+	float y = GenerateNonNullPrimeNumber();
+	float z = GenerateNonNullPrimeNumber();
+	objectsData[i*4] = sphereSizes[i];
 	objectsData[i*4 + 1] = x;
-	printf("objectsData[%d] = %.5f\n", i + 1, kSphereRadiuses[i + 1]);
-
 	objectsData[i*4 + 2] = y;
-	printf("objectsData[%d] = %.5f\n", i + 2, kSphereRadiuses[i + 2]);
-
 	objectsData[i*4 + 3] = z;
-	printf("objectsData[%d] = %.5f\n", i + 3, kSphereRadiuses[i + 3]);
-      }
-    
-      printf("#######################################\n");
-      printf("Scatter Data\n");
-      for (int i = 0; i < worldSize; ++i)
+
+	if (i == worldRank)
 	{
-	  printf("\tSphere radius: %.5f\n", objectsData[i*4]);
-	  printf("\tx: %.5f\n", objectsData[i*4 + 1]);
-	  printf("\ty: %.5f\n", objectsData[i*4 + 2]);
-	  printf("\tz: %.5f\n", objectsData[i*4 + 3]);
-	}
-      printf("#######################################\n");
+	  myRankX = x;
+	  myRankY = y;
+	  myRankZ = z;
+        }
+      }
     }
     float* objectData = new float[4];
     MPI_Scatter(objectsData, 4, MPI_FLOAT, objectData, 4, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
     MPI_Allgather(objectData, 4, MPI_FLOAT, readBuf, 4, MPI_FLOAT, MPI_COMM_WORLD);
 
-    for (int i = 0; i < worldSize*4; i+=4)
+    for (int i = 0, rank = 0; i < worldSize*4; i+=4, ++rank)
     {
-      printf("Rank %d: Sphere size %.5f, Sphere coordinates( %.5f, %.5f, %.5f )", worldRank, readBuf[i], readBuf[i+1], readBuf[i+2], readBuf[i+3]);
-	printf("\n");
+      if (rank != worldRank)
+      {
+	float euclideanDistance = std::sqrt((myRankX - readBuf[i + 1]) * (myRankX - readBuf[i + 1]) +
+					    (myRankY - readBuf[i + 2]) * (myRankY - readBuf[i + 2]) +
+					    (myRankZ - readBuf[i + 3]) * (myRankZ - readBuf[i + 3])
+					    );
+	float summedRadiuses = readBuf[i] + sphereSizes[worldRank];
+	
+	if (euclideanDistance <= summedRadiuses && !IsPreviouslyFoundCollision(rank, worldRank))
+	{
+	  auto pointOfCollision = std::chrono::high_resolution_clock::now();
+	  auto collisionTimestamp = std::chrono::duration_cast<std::chrono::microseconds>(pointOfCollision - collisionStart).count();
+	  printf("@%dus Sphere %d colided with sphere %d.\n", collisionTimestamp, rank, worldRank);
+	  collisions.push_back(std::make_tuple(rank, worldRank));
+	}
+      }	
     }
-    sleep(2);
   }
 
   if (nullptr != readBuf) free(readBuf);
